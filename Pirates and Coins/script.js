7efc2104dd645f19d25f0a7f7644a724bd9abf62
Orkhan Ismayilov <34453008+orkhanismayilov@users.noreply.github.com>
2017-12-17 12:58:19 +0400

    Homework #4.04 v1.01

--- Pirates and Coins/script.js
+++ Pirates and Coins/script.js
@@ -2,17 +2,22 @@ var m = parseInt(Number(prompt("Enter amount of coins")));
 console.log("Total amount of coins: " + m + " coins.");
 var a = parseInt(Number(prompt("How many coins took the youngest pirate?")));
 console.log("The youngest pirate took: " + a + " coins.")
-var x = 1;
+var x = 0;
 var z = parseInt(m / 3);
 
-if (m >= 0 && a >= 0 && a < z) {
-    while (a < z) {
+if (m > 0 && a > 0 && a < z) {
+    while (a <= z) {
+        z -= a;
         a++;
+        x++;
     }
-    x++;
-    console.log("Total number of pirates: " + (x + 1));
-    console.log(z % (x + 1) + " Coins left.");
+    // console.log(z);
+    if (z == 0) {
+        console.log("Total number of pirates: " + (x + 1));
+    }else{
+        console.warn(" Wrong given data!");
+    }
 
 } else {
-    console.log("Wrong given data!");
+    console.warn(" Wrong given data!");
 }
